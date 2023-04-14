bba5fe19a9c3176c34b6f31b157796be21cf5b3f
Antonia Sisneros <antonia.sisneros@linquest.com>
2023-04-14 08:10:27 -0600

    handled keyboard interrupt

--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
-from Building import Building
-import config
 import argparse
 import time
+import sys
+import os
+import config
+from Building import Building
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--floors', type=int,
-            help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_FLOORS), 
-            default=config.DEFAULT_NUM_FLOORS)
+        help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_FLOORS), 
+        default=config.DEFAULT_NUM_FLOORS)
     parser.add_argument('-e', '--elevators', type=int,
-            help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_ELEVATORS), 
-            default=config.DEFAULT_NUM_ELEVATORS)
+        help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_ELEVATORS), 
+        default=config.DEFAULT_NUM_ELEVATORS)
     args = parser.parse_args()
 
     print('Starting simulation with {0} floors and {1} elevators!'.format(args.floors, args.elevators))
@@ -23,4 +25,11 @@ def main():
 
 
 if __name__ == "__main__":
+  try:
     main()
+  except KeyboardInterrupt:
+    print('Interrupted')
+    try:
+      sys.exit(130)
+    except SystemExit:
+      os._exit(130)
