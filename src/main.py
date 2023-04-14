e947783edea5f3bcdc3d49d29527de2a5924ec6a
Antonia Sisneros <antonia.sisneros@linquest.com>
2023-04-14 08:05:49 -0600

    added command line options

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from Building import Building
 import config
+import argparse
 import time
 
 def main():
-    print('hello world')
-    # parse args
-    # TODO: for now using hard coded vals
-    NUM_FLOORS = 5
-    NUM_ELEVATORS = 2
-    building = Building(NUM_FLOORS, NUM_ELEVATORS)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--floors', type=int,
+            help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_FLOORS), 
+            default=config.DEFAULT_NUM_FLOORS)
+    parser.add_argument('-e', '--elevators', type=int,
+            help="number of floors to simulate, default is {0}".format(config.DEFAULT_NUM_ELEVATORS), 
+            default=config.DEFAULT_NUM_ELEVATORS)
+    args = parser.parse_args()
+
+    print('Starting simulation with {0} floors and {1} elevators!'.format(args.floors, args.elevators))
+    building = Building(args.floors, args.elevators)
 
     # generate a new passenger every NEW_PASSENGER_TIMEOUT ms
     while True:
